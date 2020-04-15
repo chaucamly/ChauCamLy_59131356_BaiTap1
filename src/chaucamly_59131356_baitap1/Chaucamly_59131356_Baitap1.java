f4ea656b4ecb02519e693509a0e6d3e6e4e97754
chaucamly <ly.cc.59cntt@ntu.edu.vn>
2020-04-15 08:57:21 +0700

    init

---
+++ src/chaucamly_59131356_baitap1/Chaucamly_59131356_Baitap1.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chaucamly_59131356_baitap1;
+
+/**
+ *
+ * @author Cam Ly
+ */
+public class Chaucamly_59131356_Baitap1 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
